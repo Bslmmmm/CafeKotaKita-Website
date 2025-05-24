325167d26a5b327934e20f4c80af01ad6f6c01d3
LouisSoe <loungoding@gmail.com>
2025-05-24 13:05:07 +0700

    Make 'nama' field unique in users table migration

--- database/migrations/0001_01_01_000000_create_users_table.php
+++ database/migrations/0001_01_01_000000_create_users_table.php
@@ -13,7 +13,7 @@ return new class extends Migration
     {
         Schema::create('users', function (Blueprint $table) {
             $table->uuid('id')->primary();
-            $table->string('nama');
+            $table->string('nama')->unique();
             $table->string("no_telp")->nullable();
             $table->string("alamat")->nullable();
             $table->string('email')->unique();
